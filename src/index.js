16cd9a5f7d581978873998bcb596ca9ccffe6e06
duhailong <duhailong@hujiang.com>
2019-05-25 13:24:30 +0800

    add libs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import 'whatwg-fetch';
+import 'promise-polyfill/src/polyfill';
 import store, { history } from './shares/store';
 import * as serviceWorker from './shares/serviceWorker';
 import Home from './pages/Home';
